d467bb1d853eaa0768c8946a36ceef832d141b08
jongkyung.woo <jkwoo@gooroom.kr>
2017-03-09 11:39:19 +0900

    disable selinux-activate
    Change-Id: I67ff7d271a9498e2e1f1ed84105ab8681a5fe4e5

--- usr/lib/live-installer/installer.py
+++ usr/lib/live-installer/installer.py
@@ -508,9 +508,9 @@ class InstallerEngine:
         self.do_run_in_chroot("sed -i 's/^deb cdrom/#deb cdrom/' /etc/apt/sources.list")
         self.do_run_in_chroot("apt-get -y --force-yes autoremove")
 
-        # Enable selinux
-        print " ---> Enable SELinux"
-        self.do_run_in_chroot("/usr/sbin/selinux-activate")
+        ## Enable selinux
+        #print " ---> Enable SELinux"
+        #self.do_run_in_chroot("/usr/sbin/selinux-activate")
 
         # now unmount it
         print " --> Unmounting partitions"
