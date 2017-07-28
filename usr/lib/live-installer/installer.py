886a051ffd50cd6a23055db1ea2c2ab6c0691ccf
jongkyung.woo <jkwoo@gooroom.kr>
2017-07-28 19:28:34 +0900

    Added encrypt-home option at adduser
    Change-Id: I9c0c8e2685bfd15f97a2e0186ec6ccc55cc8717e

--- usr/lib/live-installer/installer.py
+++ usr/lib/live-installer/installer.py
@@ -297,7 +297,13 @@ class InstallerEngine:
         print " --> Adding new user"
         our_current += 1
         self.update_progress(total=our_total, current=our_current, message=_("Adding new user to the system"))
-        self.do_run_in_chroot('adduser --disabled-login --gecos "{real_name}" {username}'.format(real_name=setup.real_name.replace('"', r'\"'), username=setup.username))
+
+        # ecryptfs-utils
+        if os.path.exists('/target/sbin/mount.ecryptfs'):
+            self.do_run_in_chroot('adduser --disabled-login --gecos "{real_name}" --encrypt-home {username}'.format(real_name=setup.real_name.replace('"', r'\"'), username=setup.username))
+        else:
+            self.do_run_in_chroot('adduser --disabled-login --gecos "{real_name}" {username}'.format(real_name=setup.real_name.replace('"', r'\"'), username=setup.username))
+
         for group in 'adm audio bluetooth cdrom dialout dip fax floppy fuse lpadmin netdev plugdev powerdev sambashare scanner sudo tape users vboxusers video'.split():
             self.do_run_in_chroot("adduser {user} {group}".format(user=setup.username, group=group))
 
