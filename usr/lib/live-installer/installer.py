bc64985a0df0a160a6a3d55898ad51fd367fd1b1
jongkyung.woo <jkwoo@gooroom.kr>
2018-10-26 14:30:46 +0900

    Use the dpkg command to install EFI related packages
    - The apt command tries to install the grub in the repository
      rather than the local packages.
    
    Change-Id: I425fa9911e449b34e16d550b6c350c81785f9779

--- usr/lib/live-installer/installer.py
+++ usr/lib/live-installer/installer.py
@@ -229,15 +229,15 @@ class InstallerEngine:
 
         if (setup.gptonefi):
             print " --> Creating /target/boot/efi/EFI/gooroom/grubx64.efi"
-            #os.system("mkdir -p /target/boot/efi/EFI/gooroom")
-            #os.system("cp /run/live/medium/EFI/BOOT/grubx64.efi /target/boot/efi/EFI/gooroom")
+            os.system("mkdir -p /target/boot/efi/EFI/gooroom")
+            os.system("cp /run/live/medium/EFI/BOOT/grubx64.efi /target/boot/efi/EFI/gooroom")
             os.system("mkdir -p /target/debs")
             os.system("cp /run/live/medium/pool/main/g/grub2/grub-efi* /target/debs/")
             os.system("cp /run/live/medium/pool/main/e/efibootmgr/efibootmgr* /target/debs/")
             os.system("cp /run/live/medium/pool/main/e/efivar/* /target/debs/")
             os.system("cp /run/live/medium/pool/main/s/shim/* /target/debs/")
             self.do_run_in_chroot("DEBIAN_FRONTEND=noninteractive dpkg -P grub-pc grub-pc-bin grub2")
-            self.do_run_in_chroot("apt install --yes /debs/*")
+            self.do_run_in_chroot("dpkg -i /debs/*.deb")
 
             if(not os.path.exists("/target/boot/efi/EFI/gooroom/grubx64.efi")):
                 #
