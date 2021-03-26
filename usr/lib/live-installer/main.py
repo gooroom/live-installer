6d003ae1f3a5b1fc6dd150c65274b742c8f0fb8f
boyeon.choi <boyeon.choi@gooroom.kr>
2021-03-26 14:10:34 +0900

    [Bug:3378] Fixed to run only one process
    Change-Id: I4fa5a56bfa530787a630e9fa720d17c7178ad4f1

--- usr/lib/live-installer/main.py
+++ usr/lib/live-installer/main.py
@@ -15,8 +15,29 @@ from gi.repository import Gtk
 
 # main entry
 if __name__ == "__main__":
-	if("install" in commands.getoutput("cat /proc/cmdline")):
-		win = InstallerWindow(fullscreen=True)
-	else:
-		win = InstallerWindow(fullscreen=False)
-	Gtk.main()
+    #Just one window
+    num_live_installer = commands.getoutput("ps -A | grep liveInstaller | wc -l")
+    if (num_live_installer is not "0"):
+        quit()
+
+    #Set process name
+    architecture = commands.getoutput("uname -a")
+    if (architecture.find("x86_64") >= 0):
+        import ctypes
+        libc = ctypes.CDLL('libc.so.6')
+        libc.prctl(15, 'liveInstaller', 0, 0, 0)
+    else:
+        import dl
+        if os.path.exists('/lib/libc.so.6'):
+            libc = dl.open('/lib/libc.so.6')
+            libc.call('prctl', 15, 'liveInstaller', 0, 0, 0)
+        elif os.path.exists('/lib/i386-linux-gnu/libc.so.6'):
+            libc = dl.open('/lib/i386-linux-gnu/libc.so.6')
+            libc.call('prctl', 15, 'liveInstaller', 0, 0, 0)
+
+    if("install" in commands.getoutput("cat /proc/cmdline")):
+        win = InstallerWindow(fullscreen=True)
+    else:
+        win = InstallerWindow(fullscreen=False)
+
+    Gtk.main()
